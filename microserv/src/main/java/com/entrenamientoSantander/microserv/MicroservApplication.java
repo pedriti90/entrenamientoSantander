665b62b13b03fefb360a795f91253c9ab1944b96
Pedro Gonzalez <pedriti90@gmail.com>
2020-04-15 08:39:06 +0200

    Primera subida al repositorio

---
+++ microserv/src/main/java/com/entrenamientoSantander/microserv/MicroservApplication.java
@@ -0,0 +1,13 @@
+package com.entrenamientoSantander.microserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservApplication.class, args);
+	}
+
+}
